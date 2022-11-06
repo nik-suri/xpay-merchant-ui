ee14c0e71cca93b30451ba46d51592fee690d713
Nikhil Suri <nikhilsuri@comcast.net>
2022-11-05 18:02:18 -0700

    fixed url

--- src/components/Home.js
+++ src/components/Home.js
@@ -85,6 +85,8 @@ function Home(props) {
     </Button>
   );
 
+  const merchantXPayUrl = merchantId ? `http://xpay-ui.s3-website-us-east-1.amazonaws.com/#/?merchantId=${merchantId}` : "";
+
   const merchantInstructions = merchantId ? (
     <div style={{ marginTop: "20px" }}>
       <Paragraph>
@@ -100,7 +102,7 @@ function Home(props) {
         In the "Payment instructions" section, paste the following:
       </Paragraph>
       <Paragraph strong>
-        Thank you for choosing to pay with xPay! Please navigate to <Link href={`https://xpay.com/?merchantId=${merchantId}`}>http://xpay-ui.s3-website-us-east-1.amazonaws.com/#/?merchantId={merchantId}</Link> to complete your payment. 
+        Thank you for choosing to pay with xPay! Please navigate to <Link href={merchantXPayUrl}>{merchantXPayUrl}</Link> to complete your payment. 
       </Paragraph>
       <Paragraph strong>
         Once you are on xPay, please correctly enter the USD amount of the item that you just purchased and the order ID that you see above. If you do not enter the correct amount or order ID, your order will not be processed.
