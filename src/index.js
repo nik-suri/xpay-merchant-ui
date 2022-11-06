fadada82f788642d1f35d0cebb89b5688146d34d
Nikhil Suri <nikhilsuri@comcast.net>
2022-11-05 19:21:12 -0700

    remove react router

--- src/index.js
+++ src/index.js
@@ -2,23 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Root from "./Root";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Root />
   </React.StrictMode>
 );
 
